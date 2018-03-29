7bc119f8610d4bff87c9f0b5da32f6d52e3f4e68
hassanChmais <hassan.chmaiss@gmail.com>
2018-03-30 00:12:22 +0300

    plus

--- resources/views/matches/index.blade.php
+++ resources/views/matches/index.blade.php
@@ -5,7 +5,7 @@
  <div class="row">
         <div class="col-md-12 ">
             <div class="panel panel-default">
-                <div class="panel-heading">Add Matches</div>
+                <div class="panel-heading">Add Matchess</div>
 
                 <div class="panel-body">
 <form method="POST" enctype="multipart/form-data" class="well">
