94b73ea5cf88bd9301ff6eca794737b749e32a9b
hassanChmais <hassan.chmaiss@gmail.com>
2018-03-30 18:10:39 +0300

    web

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::get('/', function () {
 });
 
 Route::get('api/get_all_teams','ApiController@get_all_teams')->name('get_all_teams');
+Route::get('api/get_all_groups','ApiController@get_all_groups')->name('get_all_groups');
 Route::middleware('auth')->group(function () {
 	//Rounds
 Route::get('/addRound', 'RoundController@index')->name('AddRound');
